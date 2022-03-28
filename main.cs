e6888a12ec3f993e7d87b26ca44b7c9bec606772
MT-Aiden <99355180+MT-Aiden@users.noreply.github.com>
2022-03-28 03:18:17 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,15 @@
+// Created by: Aiden McLeod
+// Created on: March 2022
+//
+// This program displays, "Hello, World!"
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("\nDone.");
+    }
+}
